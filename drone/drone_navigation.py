619d0c8927cb4f5e7cd7a5395a622ae9580080f9
summerof15 <zhangkai11@11.com>
2021-01-31 09:34:39 +0100

    update drone navigation

--- drone/drone_navigation.py
+++ drone/drone_navigation.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+from __future__ import print_function
+
 import rospy
 from geometry_msgs.msg import Twist
 from hector_uav_msgs.srv import EnableMotors
@@ -6,7 +8,27 @@ from get_drone_position import Block,State
 import math
 
 
-nodes=[[-1,2,0],[-16,2,0],[-27,2,0],[-27,11,0],[-18,11,0],[-5,11,0],[-5,6,0],[7,6,0],[7,2,0],[1,2,0]]
+nodes = [[0.0,2.0,1],
+        [7.0,1.0,1],
+        [6.5,8.5,1],
+        [-4.0,8.5,1],
+        [-4.0,12.5,1],
+        [-16.0,12.5,1],
+        [-16.0,3.5,1,0],
+        [-4.0,3.5,1,0]]
+
+
+def angle_wrap(a):
+
+    ## limit the angle between 0 and pi ##
+     
+    while(a>math.pi):
+        a = a - 2*math.pi
+    while(a <= -math.pi):
+        a = a + 2*math.pi
+
+    return a
+
 def take_off(drone_N):
     # Starts a new node
     assert int(drone_N) in {1,2,3,4}
@@ -21,11 +43,12 @@ def take_off(drone_N):
         show = State()
         # Receiveing the user's input
         # speed = (input("Input your vertical_velocity: "))
-        speed=0.5
-        alpha=0.1
+        krho=0.5
+        kalpha=0.5
+        speed=0.1
         # distance = (input("Type your distance: "))
 
-        distance=1.5
+        take_off_distance=1
         vel_msg.linear.z = 0
 
         vel_msg.linear.x = 0
@@ -41,52 +64,73 @@ def take_off(drone_N):
             current_distance = 0
 
             # Loop to move the turtle in an specified distance
-            while abs(current_distance - distance)>0.05 and not rospy.is_shutdown():
+            while abs(current_distance - take_off_distance)>0.2 and not rospy.is_shutdown():
 
                 # Takes actual time to velocity calculus
                 t1 = rospy.Time.now().to_sec()
                 # Calculates distancePoseStamped
-                resp_coordinates=show.show_drone_position(drone_N)
-                current_distance=resp_coordinates.pose.position.z
-
-                vel_msg.linear.z = alpha*abs(current_distance - distance)
+                position=show.show_drone_position(drone_N)
+                current_distance=position[2]
+                print(vel_msg)
+                vel_msg.linear.z = krho*abs(current_distance - take_off_distance)
+                vel_msg.linear.x = 0
+                vel_msg.linear.y = 0
+                vel_msg.angular.x = 0
+                vel_msg.angular.y = 0
+                vel_msg.angular.z = 0
                 velocity_publisher.publish(vel_msg)
+                print(vel_msg)
 
                 # print("{},{}".format(resp_coordinates.pose.position.x,resp_coordinates.pose.position.y))
 
                 # print(current_distance)
 
             # After the loop, stops the robot
-            
-            vel_msg.linear.z = 0.
-            # Force the robot to stop
-            velocity_publisher.publish(vel_msg)
-            print("ccc")
+            print("take off ready")
 
             for node in nodes:
                 print("current target{}".format(node))
-                resp_coordinates=show.show_drone_position(drone_N)
-                relative_vector=[(node[0]-resp_coordinates.pose.position.x),node[1]-resp_coordinates.pose.position.y]
+                position=show.show_drone_position(drone_N)
+                relative_vector=[node[0]-position[0],node[1]-position[1]]
                 distance=math.sqrt(relative_vector[0]**2+relative_vector[1]**2)
-                
-                vel_msg.linear.x=speed*relative_vector[0]
-                vel_msg.linear.z = 0
-                vel_msg.linear.y = speed*relative_vector[1]
-                vel_msg.angular.x = 0
-                vel_msg.angular.y = 0
-                vel_msg.angular.z = 0
-
-
-                # Loop to move the turtle in an specified distance
-                while (distance>0.1) and not rospy.is_shutdown():
-
-                    resp_coordinates=show.show_drone_position(drone_N)
-                    relative_vector=[(node[0]-resp_coordinates.pose.position.x),node[1]-resp_coordinates.pose.position.y]
+                AngleToGoal = angle_wrap(math.atan2(relative_vector[1], relative_vector[0])-position[3])
                     
+                turn = kalpha * AngleToGoal
+                while abs(AngleToGoal)>0.1 and not rospy.is_shutdown():
+                    position=show.show_drone_position(drone_N)
+                    relative_vector=[node[0]-position[0],node[1]-position[1]]
                     distance=math.sqrt(relative_vector[0]**2+relative_vector[1]**2)
-                    vel_msg.linear.x=speed*relative_vector[0]
-                    vel_msg.linear.y = speed*relative_vector[1]
+                    AngleToGoal = angle_wrap(math.atan2(relative_vector[1], relative_vector[0])-position[3])
+                    # print("{},{}".format(relative_vector[0],relative_vector[1]))
 
+                    turn = kalpha * AngleToGoal
+                    vel_msg.linear.x=0
+                    vel_msg.linear.y = 0
+                    vel_msg.linear.z = 0
+                    vel_msg.angular.z=turn
+                    velocity_publisher.publish(vel_msg)
+
+                position=show.show_drone_position(drone_N)
+                relative_vector=[node[0]-position[0],node[1]-position[1]]
+                distance=math.sqrt(relative_vector[0]**2+relative_vector[1]**2)
+                # Loop to move the turtle in an specified distance
+                i=0
+                while (distance>0.3) and not rospy.is_shutdown():
+                    i+=1
+                    position=show.show_drone_position(drone_N)
+                    relative_vector=[node[0]-position[0],node[1]-position[1]]
+                    distance=math.sqrt(relative_vector[0]**2+relative_vector[1]**2)
+                    AngleToGoal = angle_wrap(math.atan2(relative_vector[1], relative_vector[0])-position[3])
+                    # if(i%20==0):
+                        # print("{},{}".format(relative_vector[0],relative_vector[1]))
+                    turn = kalpha * AngleToGoal
+                    vel_msg.linear.x=krho*distance
+                    vel_msg.linear.y = 0
+                    vel_msg.linear.z = 0
+                    vel_msg.angular.x=0
+                    vel_msg.angular.y=0
+                    vel_msg.angular.z=turn
+                    # print(vel_msg)
                     velocity_publisher.publish(vel_msg)
 
         else:
@@ -109,7 +153,6 @@ if __name__ == '__main__':
     print("Let's move your drone")
     # drone_N = input("Select a drone to move. (Options 1, 2, 3, 4): ")
     # Testing our function
-    drone_N=1
+    drone_N=2
     
     take_off(drone_N)
-    
